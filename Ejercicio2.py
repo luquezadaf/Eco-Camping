739c094c5be59989d25d11b1eb9fa0ee3cf0cbbf
luquezadaf <lu.quezadaf@duocuc.cl>
2026-05-26 21:27:50 -0400

    Add files via upload

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -20,7 +20,23 @@ while ejecutando:
         disponibles = capacidad_maxima - sitios_ocupados
         print(f"Actualmente hay {disponibles} sitios disponibles")
     elif opcion == 2:
+        sitios_libres = capacidad_maxima - sitios_ocupados
+        if sitios_libres == 0:
+            print("Lo sentimos, no quedan espacios en el camping")
+        else:
+            try:
+                ingreso = int(input("¿Cuantos sitios o vehiculos van a ingresar?"))
+                if ingreso <= 0:
+                    print("Error: La cantidad debe ser mayor a 0")
+                elif ingreso > sitios_libres:
+                    print(f"Solo puede ingresar un máximo de {sitios_libres} sitios libres")
+                else:
+                    sitios_ocupados += ingreso
+                    print(f"Ingreso registrado. Se han ocupado {ingreso} de sitios")
+            except ValueError:
+                print("Error: debe ingresar un número valido")
     elif opcion == 3:
+        
     elif opcion == 4:
     elif opcion == 5:
     else:
